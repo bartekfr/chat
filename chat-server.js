6dbb0af9c7768332f6ee66106980d15ffe58a3f5
Bartek Fryzowicz <bartekfr@wp.pl>
2015-03-23 11:33:13 +0100

    init app

---
+++ chat-server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(callback).listen(3000);
+var websocket = require('socket.io').listen(server);
+
+function callback(req, res) {
+	fs.readFile(__dirname + '/index.html',
+		function(error, data) {
+			if (error) {
+			 res.writeHead(500);
+			 return res.end('Error loading index.html');
+			}
+
+			res.writeHead(200);
+			res.end(data);
+		}
+	);
+}
+
+websocket.sockets.on('connection', function (socket) {
+	socket.on('clientMessage', function(content) {
+		socket.emit('serverMessage', 'You said: ' + content);
+		socket.broadcast.emit('serverMessage', socket.id + ' said: ' + content);
+	});
+});
