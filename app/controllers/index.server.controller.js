d4923438aad7b24b57fa8570397493e098cab14d
Bartek Fryzowicz <bartekfr@wp.pl>
2015-03-26 15:01:43 +0100

    ejs views

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,7 @@
 var path = require("path");
 exports.render = function(req, res) {
-	res.sendFile(path.join(__dirname, '../../', 'index.html'));
+//	res.sendFile(path.join(__dirname, '../../', 'index.html'));
+	res.render('index', {
+		title: 'Chat app'
+	})
 };
