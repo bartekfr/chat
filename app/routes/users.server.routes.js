2d8bb8733df60e93f9547d573fb9aa5a1e8a7284
Bartek Fryzowicz <bartekfr@wp.pl>
2015-03-30 18:46:07 +0200

    databes read, userById middleware

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,4 +1,11 @@
 module.exports = function(app) {
 	var users = require('../controllers/users.server.controller');
-	app.route('/users').post(users.create);
+	app.route('/users')
+		.post(users.create)
+		.get(users.list);
+
+	app.route('/users/:userId')
+		.get(users.read);
+
+	app.param('userId', users.userByID);
 };
